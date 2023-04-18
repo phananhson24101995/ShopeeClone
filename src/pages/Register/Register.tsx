bec67198c5cd8e32bd7998b6f335e5cffbccd250
Phan Anh Son <phananhson24101995@gmail.com>
2023-04-19 00:36:55 +0700

    Validate Register Form với React Hook Form

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,42 +1,64 @@
 import { Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+import { rules } from 'src/utils/rules'
+
+interface StateFormType {
+  email: string
+  password: string
+  confirm_password: string
+}
 
 function Register() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<StateFormType>()
+
+  const onSubmit = (data: any, e: any) => {
+    console.log('data', data)
+  }
+
+  console.log('errors', errors)
   return (
     <div className='bg-orange'>
       <div className='mx-auto max-w-7xl px-4'>
         <div className='grid grid-cols-1 py-12 lg:grid-cols-5 lg:py-32 lg:pr-10'>
           <div className='lg:col-span-2 lg:col-start-4'>
-            <form className='rounded bg-white p-10 shadow-sm'>
+            <form className='rounded bg-white p-10 shadow-sm' onSubmit={handleSubmit(onSubmit)} noValidate>
               <div className='text-2xl'>Đăng ký</div>
               <div className='mt-8'>
                 <input
+                  {...register('email', rules.email)}
                   type='email'
-                  name='email'
                   className='w-full rounded-sm border border-gray-300 p-3 outline-none focus:border-gray-500 focus:shadow-sm'
                   placeholder='Email...'
                 />
-                {/* <div className='mt-1 min-h-[1rem] text-sm text-red-600'>Email không hợp lệ</div> */}
+                <div className='mt-1 min-h-[1.25rem] text-sm text-red-600'>{errors.email?.message}</div>
               </div>
-              <div className='mt-3'>
+              <div className='mt-2'>
                 <input
+                  {...register('password', rules.password)}
                   type='password'
-                  name='confirm_password'
                   className='w-full rounded-sm border border-gray-300 p-3 outline-none focus:border-gray-500 focus:shadow-sm'
                   placeholder='Password...'
                 />
-                {/* <div className='mt-1 min-h-[1rem] text-sm text-red-600'>Mật khẩu không hợp lệ</div> */}
+                <div className='mt-1 min-h-[1.25rem] text-sm text-red-600'>{errors.password?.message}</div>
               </div>
-              <div className='mt-3'>
+              <div className='mt-2'>
                 <input
+                  {...register('confirm_password', rules.confirm_password)}
                   type='password'
-                  name='password'
                   className='w-full rounded-sm border border-gray-300 p-3 outline-none focus:border-gray-500 focus:shadow-sm'
                   placeholder='Confirm password...'
                 />
-                {/* <div className='mt-1 min-h-[1rem] text-sm text-red-600'>Mật khẩu không hợp lệ</div> */}
+                <div className='mt-1 min-h-[1.25rem] text-sm text-red-600'>{errors.confirm_password?.message}</div>
               </div>
               <div className='mt-3'>
-                <button className='w-full bg-red-500 px-2 py-4 text-center text-sm uppercase text-white hover:bg-red-600'>
+                <button
+                  type='submit'
+                  className='w-full bg-red-500 px-2 py-4 text-center text-sm uppercase text-white hover:bg-red-600'
+                >
                   Đăng ký
                 </button>
               </div>
