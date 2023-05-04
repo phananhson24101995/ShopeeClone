836ee566d6e3ec609ca8d54e0bbf02706799e1d8
Phan Anh Son <phananhson24101995@gmail.com>
2023-05-04 16:50:31 +0700

    183. Code logic cho SortProductList component

--- src/pages/ProductList/SortProductList/SortProductList.tsx
+++ src/pages/ProductList/SortProductList/SortProductList.tsx
@@ -1,45 +1,146 @@
 import { ChevronLeft, ChevronRight } from 'src/icons'
+import { QueryConfig } from '../ProductList'
+import { sortBy, order as orderConstant } from 'src/constants/product'
+import classNames from 'classnames'
+import { ProductListConfig } from 'src/types/product.type'
+import { Link, createSearchParams, useNavigate } from 'react-router-dom'
+import path from 'src/constants/path'
+import { omit } from 'lodash'
+interface Props {
+  queryConfig: QueryConfig
+  pageSize: number
+}
+export default function SortProductList({ pageSize, queryConfig }: Props) {
+  const { sort_by = sortBy.createdAt, order } = queryConfig
+  const page = Number(queryConfig.page)
+  const navigate = useNavigate()
 
-export default function SortProductList() {
+  const isActiveSortBy = (sortByValue: Exclude<ProductListConfig['sort_by'], undefined>) => {
+    return sort_by === sortByValue
+  }
+
+  const handleSort = (sortByValue: Exclude<ProductListConfig['sort_by'], undefined>) => {
+    navigate({
+      pathname: path.home,
+      search: createSearchParams(
+        omit(
+          {
+            ...queryConfig,
+            sort_by: sortByValue
+          },
+          ['order']
+        )
+      ).toString()
+    })
+  }
+
+  const handlePriceOrder = (sortByValue: Exclude<ProductListConfig['order'], undefined>) => {
+    navigate({
+      pathname: path.home,
+      search: createSearchParams({
+        ...queryConfig,
+        order: sortByValue,
+        sort_by: sortBy.price
+      }).toString()
+    })
+  }
+
   return (
     <div className='bg-gray-300/40 px-3 py-4'>
       <div className='flex flex-wrap items-center justify-between gap-2'>
         <div className='flex flex-wrap items-center gap-2'>
           <div>Sắp xếp theo</div>
-          <button className='h-8 bg-orange px-4 text-center text-sm capitalize text-white hover:bg-orange/80'>
+          <button
+            onClick={() => handleSort(sortBy.view)}
+            className={classNames('h-8 px-4 text-center text-sm capitalize', {
+              'bg-orange text-white hover:bg-orange/80': isActiveSortBy(sortBy.view),
+              'bg-white text-black hover:bg-slate-100': !isActiveSortBy(sortBy.view)
+            })}
+          >
             Phổ biến
           </button>
-          <button className='h-8 bg-white px-4 text-center text-sm capitalize text-black hover:bg-slate-100'>
+          <button
+            onClick={() => handleSort(sortBy.createdAt)}
+            className={classNames('h-8 px-4 text-center text-sm capitalize', {
+              'bg-orange text-white hover:bg-orange/80': isActiveSortBy(sortBy.createdAt),
+              'bg-white text-black hover:bg-slate-100': !isActiveSortBy(sortBy.createdAt)
+            })}
+          >
             Mới nhất
           </button>
-          <button className='h-8 bg-white px-4 text-center text-sm capitalize text-black hover:bg-slate-100'>
+          <button
+            onClick={() => handleSort(sortBy.sold)}
+            className={classNames('h-8 px-4 text-center text-sm capitalize', {
+              'bg-orange text-white hover:bg-orange/80': isActiveSortBy(sortBy.sold),
+              'bg-white text-black hover:bg-slate-100': !isActiveSortBy(sortBy.sold)
+            })}
+          >
             Bán chạy
           </button>
           <select
-            defaultValue={''}
+            defaultValue={order || ''}
             placeholder='Giá'
-            className='h-8 bg-white px-4 text-sm capitalize text-black outline-none hover:bg-slate-100'
+            onChange={(event) => handlePriceOrder(event.target.value as Exclude<ProductListConfig['order'], undefined>)}
+            className={classNames('h-8 px-4 text-sm capitalize  outline-none', {
+              'bg-orange text-white hover:bg-orange/80': isActiveSortBy(sortBy.price),
+              'bg-white text-black hover:bg-slate-100': !isActiveSortBy(sortBy.price)
+            })}
           >
-            <option value='' disabled>
+            <option className='bg-white text-black' value='' disabled>
               Giá
             </option>
-            <option value='price:asc'>Giá: Thấp đến Cao</option>
-            <option value='price:desc'>Giá: Cao đến Thấp</option>
+            <option className='bg-white text-black' value={orderConstant.asc}>
+              Giá: Thấp đến Cao
+            </option>
+            <option className='bg-white text-black' value={orderConstant.desc}>
+              Giá: Cao đến Thấp
+            </option>
           </select>
         </div>
 
         <div className='flex items-center'>
-          <div>
-            <span className='text-orange'>1</span>
-            <span>/2</span>
+          <div className='flex items-center justify-center'>
+            <span className='text-orange'>{page}</span>
+            <span>/{pageSize}</span>
           </div>
-          <div className='ml-2'>
-            <button className='h-8 cursor-not-allowed rounded-bl-sm rounded-tl-sm bg-white/60 px-3 shadow hover:bg-slate-200'>
-              <ChevronLeft />
-            </button>
-            <button className='h-8 rounded-br-sm rounded-tr-sm bg-white px-3 shadow hover:bg-slate-200'>
-              <ChevronRight />
-            </button>
+          <div className='ml-2 flex items-center'>
+            {page === 1 ? (
+              <span className='flex h-8 cursor-not-allowed items-center justify-center rounded-bl-sm rounded-tl-sm bg-white/60 px-3 shadow'>
+                <ChevronLeft />
+              </span>
+            ) : (
+              <Link
+                to={{
+                  pathname: path.home,
+                  search: createSearchParams({
+                    ...queryConfig,
+                    page: (page - 1).toString()
+                  }).toString()
+                }}
+                className='flex h-8 cursor-pointer items-center justify-center rounded-bl-sm rounded-tl-sm bg-white px-3 shadow hover:bg-slate-200'
+              >
+                <ChevronLeft />
+              </Link>
+            )}
+
+            {page === pageSize ? (
+              <span className='flex h-8 cursor-not-allowed items-center justify-center rounded-bl-sm rounded-tl-sm bg-white/60 px-3 shadow'>
+                <ChevronRight />
+              </span>
+            ) : (
+              <Link
+                to={{
+                  pathname: path.home,
+                  search: createSearchParams({
+                    ...queryConfig,
+                    page: (page + 1).toString()
+                  }).toString()
+                }}
+                className='flex h-8 items-center justify-center rounded-br-sm rounded-tr-sm bg-white px-3 shadow hover:bg-slate-200'
+              >
+                <ChevronRight />
+              </Link>
+            )}
           </div>
         </div>
       </div>
